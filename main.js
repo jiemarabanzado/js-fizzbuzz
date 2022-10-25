29f38de92d0e0081a3e07a51d7a0837587f71e4a
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-25 09:06:16 +0200

    rislto append

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
 let a="Fizz";
 let b='Buzz';
 let c='FizzBuzz';
-var NewDiv =document.getElementsByClassName("Mydiv");
-
+var NewDiv =document.getElementById("Mydiv");
 for(let i=1 ; i<=100;i++){
-    let element=document.createElement('div');
+    let element = document.createElement('div');
     if(i%3==0 && i%5==0){
-        element.innerHTML=c;
+        element.innerHTML=`c`;
         NewDiv.append(element);
-        element.classList.add('square');
         console.log(c);
     }else if(i%5==0){
         element.innerHTML=b;
-        element.classList.add('square');
         console.log(b)
     }else if(i%3==0){
         element.innerHTML=a;
         NewDiv.append(element);
-        element.classList.add('square');
         console.log(a);
     }else{
         element.innerHTML=i;
         NewDiv.append(element);
-        element.classList.add('square');
         console.log(i);
-    }
-    
+    }    
 }
