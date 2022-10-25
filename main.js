42c8cf9d8492ba61d3fdc522dd76e8f2dc1d32fd
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-25 10:15:49 +0200

    complete

--- main.js
+++ main.js
@@ -5,17 +5,22 @@ var NewDiv =document.getElementById("Mydiv");
 for(let i=1 ; i<=100;i++){
     let element = document.createElement('div');
     if(i%3==0 && i%5==0){
-        element.innerHTML=`c`;
+        element.classList.add('both','square');
+        element.innerHTML=c;
         NewDiv.append(element);
         console.log(c);
     }else if(i%5==0){
+        element.classList.add('buzz','square');
         element.innerHTML=b;
+        NewDiv.append(element);
         console.log(b)
     }else if(i%3==0){
+        element.classList.add('fizz','square');
         element.innerHTML=a;
         NewDiv.append(element);
         console.log(a);
     }else{
+        element.classList.add('normal','square');
         element.innerHTML=i;
         NewDiv.append(element);
         console.log(i);
